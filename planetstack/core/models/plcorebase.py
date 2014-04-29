44739543545426ccdaf93b40567e36d113f11d88
Sapan Bhatia <gwsapan@gmail.com>
2014-04-29 00:41:39 -0400

    Make default value of status field "pending"

--- planetstack/core/models/plcorebase.py
+++ planetstack/core/models/plcorebase.py
@@ -22,7 +22,7 @@ class PlCoreBase(models.Model):
     updated = models.DateTimeField(auto_now=True)
     enacted = models.DateTimeField(null=True, default=None)
     backend_status = models.CharField(max_length=140,
-                                      default="Ok")
+                                      default="Provisioning in progress")
 
     class Meta:
         abstract = True
