2e16351a659b2fd5d476a11dd6318beabcd1a620
Tony Mack <tmack@tux.cs.princeton.edu>
2014-11-30 18:54:25 -0500

    Controller replaces Deployment

--- planetstack/openstack_observer/steps/sync_controller_sites.py
+++ planetstack/openstack_observer/steps/sync_controller_sites.py
@@ -6,32 +6,32 @@ from observer.openstacksyncstep import OpenStackSyncStep
 from core.models.site import *
 from observer.ansible import *
 
-class SyncSiteDeployments(OpenStackSyncStep):
+class SyncControllerSites(OpenStackSyncStep):
     requested_interval=0
-    provides=[SiteDeployments, Site]
+    provides=[ControllerSites, Site]
 
-    def sync_record(self, site_deployment):
+    def sync_record(self, controller_site):
 
-	template = os_template_env.get_template('sync_site_deployments.yaml')
-	tenant_fields = {'endpoint':site_deployment.deployment.auth_url,
-		         'admin_user': site_deployment.deployment.admin_user,
-		         'admin_password': site_deployment.deployment.admin_password,
+	template = os_template_env.get_template('sync_controller_sites.yaml')
+	tenant_fields = {'endpoint':controller_site.controller.auth_url,
+		         'admin_user': controller_site.controller.admin_user,
+		         'admin_password': controller_site.controller.admin_password,
 		         'admin_tenant': 'admin',
-		         'tenant': site_deployment.site.login_base,
-		         'tenant_description': site_deployment.site.name}
+		         'tenant': controller_site.site.login_base,
+		         'tenant_description': controller_site.site.name}
 
 	rendered = template.render(tenant_fields)
-	res = run_template('sync_site_deployments.yaml', tenant_fields)
+	res = run_template('sync_controller_sites.yaml', tenant_fields)
 
 	if (len(res)==1):
-		site_deployment.tenant_id = res[0]['id']
-        	site_deployment.save()
+		controller_site.tenant_id = res[0]['id']
+        	controller_site.save()
 	elif (len(res)):
 		raise Exception('Could not assign roles for user %s'%tenant_fields['tenant'])
 	else:
 		raise Exception('Could not create or update user %s'%tenant_fields['tenant'])
             
-    def delete_record(self, site_deployment):
-        if site_deployment.tenant_id:
-            driver = self.driver.admin_driver(deployment=site_deployment.deployment.name)
-            driver.delete_tenant(site_deployment.tenant_id)
+    def delete_record(self, controller_site):
+        if controller_site.tenant_id:
+            driver = self.driver.admin_driver(controller=controller_site.controller)
+            driver.delete_tenant(controller_site.tenant_id)
