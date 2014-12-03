b15c817a0f14b9414b4079911bd73239098b668a
Tony Mack <tmack@tux.cs.princeton.edu>
2014-12-02 21:08:45 -0500

    initial commit

--- planetstack/openstack_observer/steps/sync_controller_sites.py
+++ planetstack/openstack_observer/steps/sync_controller_sites.py
@@ -8,30 +8,30 @@ from observer.ansible import *
 
 class SyncControllerSites(OpenStackSyncStep):
     requested_interval=0
-    provides=[ControllerSites, Site]
+    provides=[Controller, Site]
 
-    def sync_record(self, controller_site):
+    def sync_record(self, controller):
 
 	template = os_template_env.get_template('sync_controller_sites.yaml')
-	tenant_fields = {'endpoint':controller_site.controller.auth_url,
-		         'admin_user': controller_site.controller.admin_user,
-		         'admin_password': controller_site.controller.admin_password,
+	tenant_fields = {'endpoint':controller.auth_url,
+		         'admin_user': controller.admin_user,
+		         'admin_password': controller.admin_password,
 		         'admin_tenant': 'admin',
-		         'tenant': controller_site.site.login_base,
-		         'tenant_description': controller_site.site.name}
+		         'tenant': controller.site_deployment.site.login_base,
+		         'tenant_description': controller.site_deployment.site.name}
 
 	rendered = template.render(tenant_fields)
 	res = run_template('sync_controller_sites.yaml', tenant_fields)
 
 	if (len(res)==1):
-		controller_site.tenant_id = res[0]['id']
-        	controller_site.save()
+		controller.tenant_id = res[0]['id']
+        	controller.save()
 	elif (len(res)):
 		raise Exception('Could not assign roles for user %s'%tenant_fields['tenant'])
 	else:
 		raise Exception('Could not create or update user %s'%tenant_fields['tenant'])
             
-    def delete_record(self, controller_site):
-        if controller_site.tenant_id:
-            driver = self.driver.admin_driver(controller=controller_site.controller)
-            driver.delete_tenant(controller_site.tenant_id)
+    def delete_record(self, controller):
+        if controller.tenant_id:
+            driver = self.driver.admin_driver(controller=controller)
+            driver.delete_tenant(controller.tenant_id)
